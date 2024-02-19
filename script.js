b96df523d1bf35619cc5237f285a3006c5cafcd7
Rutger <1201954@student.roc-nijmegen.nl>
2024-02-19 13:27:00 +0100

    Refactor whole shit

--- script.js
+++ script.js
@@ -1,38 +1,62 @@
 let currentGeduld;
-async function getGeduld() {
-  const response = await fetch(`https://api.rutgerpronk.com/geduld`);
-  const json = await response.json();
-  const geduld = json.geduld;
-  return geduld;
-}
-setInterval(async () => {
-  currentGeduld = await getGeduld();
-  document.getElementById("Contentness-meter").style.width =
-    currentGeduld + "%";
-  const intervalTiming = 500 + currentGeduld * 10;
-  clearInterval(currentInterval)
-  currentInterval = setInterval(timerTick, intervalTiming)
-}, 1000);
+// let countdown = 45 * 60;
+// let timerDisplay = document.querySelector("#time");
+// let timer = duration,
+//     minutes,
+//     seconds;
 
-let currentInterval;
+async function setDefaultGeduld() {
+    await fetch("https://api.rutgerpronk.com/geduld", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            newGeduld: 50
+        }),
+    });
+}
+async function getGeduld() {
+    const response = await fetch(`https://api.rutgerpronk.com/geduld`);
+    const json = await response.json();
+    return json.geduld;
+}
+
+// function timerTick() {
+//   minutes = parseInt(timer / 60, 10);
+//   seconds = parseInt(timer % 60, 10);
+//
+//   minutes = minutes < 10 ? "0" + minutes : minutes;
+//   seconds = seconds < 10 ? "0" + seconds : seconds;
+//
+//   timerDisplay.textContent = minutes + ":" + seconds;
+//
+//   if (--timer < -1) {
+//     timerDisplay.textContent = "DONE";
+//   }
+// }
 
 function timerTick() {
-  minutes = parseInt(timer / 60, 10);
-  seconds = parseInt(timer % 60, 10);
+    // TODO: logic to remove 1 second from countdown
 
-  minutes = minutes < 10 ? "0" + minutes : minutes;
-  seconds = seconds < 10 ? "0" + seconds : seconds;
-
-  display.textContent = minutes + ":" + seconds;
-
-  if (--timer < -1) {
-    display.textContent = "DONE";
-  }
+    // Recursively call timerTick based on interval
+    // calculate a number between 2/3 and 1 1/3 based on currentGeduld
+    const interval = 1000 * (2 / 3) + currentGeduld * 10 * (2 / 3)
+    setTimeout(timerTick, interval);
 }
 
+async function driver() {
+    currentGeduld = await getGeduld();
+    document.getElementById("Contentness-meter").style.width =
+        currentGeduld + "%";
+}
 
-let countdown = 45 * 60;
-let timerDisplay = document.querySelector("#time");
-var timer = duration,
-    minutes,
-    seconds;
+// This is the "main" function
+(async () => {
+    await setDefaultGeduld();
+    await driver();
+
+    // These two happen in parallel. timerTick is a recursive function. driver is called every second.
+    timerTick();
+    setInterval(driver, 1000)
+})();
