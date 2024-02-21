460c85feba09a49be5ae3bb36c2ee2e537eaeb74
Roverfloats <tygoliebers@gmail.com>
2024-02-21 10:25:36 +0100

    alot

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 let currentGeduld;
-let countdown = 45 * 60;
+let countdown = 5 * 60;
 let timerDisplay = document.querySelector("#time");
-let intro = new Audio("intro.mp3");
+let intro = document.querySelector("#Intro");
+let startMusic = document.querySelector("#StartMusic");
+let transition = document.querySelector("#Transition");
+let LoopMusic = document.querySelector("#LoopMusic");
+let knock1 = document.querySelector("#Knock");
+let knock2 = document.querySelector("#Knock2");
+let hallo = document.querySelector("#Hallo");
+let WTHIE = document.querySelector("#WTHIE");
 
 async function setDefaultGeduld() {
   currentGeduld = 50;
@@ -36,12 +43,38 @@ function timerTick() {
     timerDisplay.textContent = "00:00";
   }
 
+//   if (countdown == 300 || countdown == 130 || countdown == 90 || countdown == 10){
+//     knock1.play();
+//   }
+//   if (countdown == 230 || countdown == 195 || countdown == 63 || countdown == 47){
+//     knock2.play()
+//   }
+//   if (countdown == 220 || countdown == 100){
+//     hallo.play()
+//   }
+//   if (countdown == 0){
+//     WTHIE.play()
+//   }
  
 
   // Recursively call timerTick based on interval
   // calculate a number between 2/3 and 1 1/3 based on currentGeduld
   const interval = 1000 * (2 / 3) + currentGeduld * 10 * (2 / 3);
   setTimeout(timerTick, interval);
+}
+
+function introAndMusic() {
+    intro.play();
+    intro.onended = function(){
+        startMusic.play();
+        startMusic.onended = function(){
+            transition.play();
+            transition.onended = function(){
+                LoopMusic.loop = true;
+                Loop.play()
+            }
+        }
+    }
 }
 
 async function driver() {
@@ -53,7 +86,7 @@ async function driver() {
 // This is the "main" function
 (async () => {
   await setDefaultGeduld();
-  intro.play();
+//   introAndMusic();
 
   // These two happen in parallel. timerTick is a recursive function. driver is called every second.
   timerTick();
