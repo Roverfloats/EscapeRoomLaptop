07b726c1a844c21ccc443e0496fb87e1a313a07f
Roverfloats <tygoliebers@gmail.com>
2024-02-22 11:54:56 +0100

    possible final commit

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 let currentGeduld;
-let countdown = 5 * 60;
+let countdown = 45 * 60;
 let timerDisplay = document.querySelector("#time");
 let intro = document.querySelector("#Intro");
 let startMusic = document.querySelector("#StartMusic");
 let transition = document.querySelector("#Transition");
 let LoopMusic = document.querySelector("#LoopMusic");
-let knock1 = document.querySelector("#Knock");
+let knock1 = document.querySelector("#knock1");
 let knock2 = document.querySelector("#Knock2");
 let hallo = document.querySelector("#Hallo");
 let WTHIE = document.querySelector("#WTHIE");
+let car = document.querySelector("#car");
+let opacityAmmount;
 
 async function setDefaultGeduld() {
   currentGeduld = 50;
@@ -43,20 +45,23 @@ function timerTick() {
     timerDisplay.textContent = "00:00";
   }
 
-//   if (countdown == 300 || countdown == 130 || countdown == 90 || countdown == 10){
-//     knock1.play();
-//   }
-//   if (countdown == 230 || countdown == 195 || countdown == 63 || countdown == 47){
-//     knock2.play()
-//   }
-//   if (countdown == 220 || countdown == 100){
-//     hallo.play()
-//   }
-//   if (countdown == 0){
-//     WTHIE.play()
-//   }
- 
+  if (countdown == 300 || countdown == 130 || countdown == 90 || countdown == 10){
+    knock1.play();
+  }
+  if (countdown == 270 || countdown == 230 || countdown == 195 || countdown == 63 || countdown == 47){
+    knock2.play();
+  }
+  if (countdown == 220 || countdown == 100){
+    hallo.play();
+  }
+  if (countdown == 330){
+    car.play();
+  }
 
+  if (countdown == 0){
+    WTHIE.play();
+  }
+ 
   // Recursively call timerTick based on interval
   // calculate a number between 2/3 and 1 1/3 based on currentGeduld
   const interval = 1000 * (2 / 3) + currentGeduld * 10 * (2 / 3);
@@ -70,8 +75,8 @@ function introAndMusic() {
         startMusic.onended = function(){
             transition.play();
             transition.onended = function(){
-                LoopMusic.loop = true;
-                Loop.play()
+                LoopMusic.loop = true
+                LoopMusic.play()
             }
         }
     }
@@ -81,12 +86,23 @@ async function driver() {
   currentGeduld = await getGeduld();
   document.getElementById("Contentness-meter").style.width =
     currentGeduld + "%";
+
+    if (currentGeduld < 50){
+      opacityAmmount = ((-currentGeduld * 2) / 100) + 1;
+    }
+    else{
+      opacityAmmount = 0;
+    }
+
+  document.getElementById("eyes").style.opacity = opacityAmmount;
+
+  console.log(currentGeduld);
 }
 
 // This is the "main" function
 (async () => {
   await setDefaultGeduld();
-//   introAndMusic();
+  introAndMusic();
 
   // These two happen in parallel. timerTick is a recursive function. driver is called every second.
   timerTick();
